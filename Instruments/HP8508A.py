09427a1b606ef1dab689432a9a5195a5a91858e6
Paul Grimes <pgrimes@cfa.harvard.edu>
2018-08-14 14:55:12 -0400

    Fiddled with Vector Voltmeter code to try and make state of object consistent so it can more easily decipher the return values from the instrument.

--- Instruments/HP8508A.py
+++ Instruments/HP8508A.py
@@ -4,31 +4,47 @@ class HP8508A(Instrument.Instrument):
     '''Class for communicating with an HP 8508A Vector Voltmeter'''
     def __init__(self, resource, strict=False, idString="8508A-050"):
         """Create Vector Voltmeter object from PyVisa resource.
-     
+
         rm = pyvisa.ResourceManager()
         pm = HP8508A(rm.open_resource(<InstAddr>))
         InstAddr is the PyVisa address of the VVM - try "GPIB0::8::INSTR" by default"""
 
         super().__init__(resource, strict, idString)
-        
+
         # Set some internal state parameters to allow return values to be interpreted
         self.mode = "UNKNOWN"
         self.format = self.getFormat()
         self.average = self.getAveraging()
         self.triggersource = "UNKNOWN"
         self.triggered = False
-        
+
         # Set termination characters
         self.resource.read_termination = '\n'
         self.resource.write_termination = '\r\n'
-        
+
+        # Get current format
+        self.getFormat()
+
     def setTransmission(self):
         '''Set the VVM to output transmission data - relative amplitude (A/B) and phase (A-B)'''
         self.write("SENSE TRANSMISSION")
         self.mode = "TRANSMISSION"
 
     def setMode(self, mode):
-        '''Set the VVM measurement mode'''
+        '''Set the VVM measurement mode
+
+        mode can be any of:
+        AVOLtage, BVOLtage - A and B voltages
+        APOWer, BPOWer - A and B powers
+        PHASe - phase between A and B
+        TRANSMISSION - power and phase difference between A and B
+        GDELay - Group delay between A and B
+        SWR -
+        RHO -
+        Y
+        Z
+        CORE
+        modes can be concatenated with comma separation'''
         self.write("SENSE {:s}".format(mode))
         self.mode = mode
 
@@ -61,12 +77,13 @@ class HP8508A(Instrument.Instrument):
             CART - volts as (real, imag)
             '''
         self.write("FORMAT {:s}".format(format))
+        self.getFormat()
 
 
     def getFormat(self):
         '''Return the current output format of the VVM'''
         self.format = self.query("FORMAT?")
-        
+
         return self.format
 
 
@@ -92,14 +109,14 @@ class HP8508A(Instrument.Instrument):
         self.write("*TRG")
         self.triggered = True
 
-        
+
     def getTransmission(self):
         '''Return the data from the VVM'''
         # TODO: Get the correct "format" strings to allow parsing of datastr
 
         # Get the data
         datastr = self.getData("TRANSMISSION")
-        
+
         if self.format == "LOGARITHMIC,POLAR" or self.format == "LINEAR,POLAR":
             amp = float(datastr.split(",")[0])
             phase = float(datastr.split(",")[1])
@@ -109,7 +126,7 @@ class HP8508A(Instrument.Instrument):
             x = float(datastr.split(",")[0])
             y = float(datastr.split(",")[1])
             data = complex(x, y)
-            
+
         else: # Unknown format, probably single valued
             try:
                 # Try to convert unknown format to a float, if not return raw string
@@ -117,31 +134,31 @@ class HP8508A(Instrument.Instrument):
             except ValueError:
                 data = datastr
         return data
-    
-    
+
+
     def getUnits(self):
         ''' Returns units for format types'''
         x = str(self.format.split(",")[0])
         y = str(self.format.split(",")[1])
-        
+
         if x == "LINEAR":
             x_units = "volts"
         elif x == "LOGARITHMIC":
             x_units = "dB"
-            
+
         if y == "POLAR":
             y_units = "deg"
         elif y == "RECTANGULAR":
             y_units = "Real + Imag"
-        
+
         units = (x_units, y_units)
-        
+
         return units
-    
-    
-    def getData(self, meas):
+
+
+    def getData(self, meas=self.mode):
         '''Return the requested measurement(s) from the VVM<meas> is one of:
-        
+
         AVOLtage, BVOLtage - A and B voltages
         APOWer, BPOWer - A and B powers
         PHASe - phase between A and B
@@ -160,8 +177,23 @@ class HP8508A(Instrument.Instrument):
 
         # Get the data
         datastr = self.query("MEAS? {:s}".format(meas))
-        
+
         # Reset the triggered state
         self.triggered = False
+        # code to decipher the response according to the mode or measurement
+        # if self.format == "LOGARITHMIC,POLAR" or self.format == "LINEAR,POLAR":
+        #     amp = float(datastr.split(",")[0])
+        #     phase = float(datastr.split(",")[1])
+        #     data = (amp, phase)
+        # elif self.format == "LINEAR,RECTANGULAR" or self.format == "LOGARITHMIC,RECTANGULAR":
+        #     x = float(datastr.split(",")[0])
+        #     y = float(datastr.split(",")[1])
+        #     data = complex(x, y)
+        # else: # Unknown format, probably single valued
+        #     try:
+        #         # Try to convert unknown format to a float, if not return raw string
+        #         data = float(datastr)
+        #     except ValueError:
+        #         data = datastr
 
         return datastr
